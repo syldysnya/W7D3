ae6460529f532386c8091d2f0e71d12b588e1582
syldysnya <syldysnya@gmail.com>
2021-04-07 16:38:10 -0400

    Add rspecs for users_controller

--- Capies/cute_capies/app/controllers/users_controller.rb
+++ Capies/cute_capies/app/controllers/users_controller.rb
@@ -1,4 +1,33 @@
 class UsersController < ApplicationController
 
-    
+    def new
+        @user = User.new
+        render :new
+    end
+
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            redirect_to user_url(@user)
+        else
+            render :new
+        end
+    end
+
+    def show
+        @user = User.find(params[:id])
+
+        if @user
+            redirect_to user_url(@user)
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
 end
--- Capies/cute_capies/spec/controllers/users_controller_spec.rb
+++ Capies/cute_capies/spec/controllers/users_controller_spec.rb
@@ -10,20 +10,20 @@ RSpec.describe UsersController, type: :controller do
 
     describe "#create" do 
         it "with invalid params, renders the new template" do 
-            post :create, user: { username: "superman" }
+            post :create, params: { user: { username: "superman" } }
             expect(response).to render_template(:new)
         end
         it "with valid params, renders the new template" do 
-            post :create, user: { username: "superman", password: "password" }
+            post :create, params: { user: { username: "superman", password: "password" } }
             expect(response).to redirect_to(user_url(User.find_by(username: "superman")))
         end
     end
 
     describe "#show" do 
-        it "if successful, renders show page" do 
-            User.create!(username: "superman", password: "password")
-            get :show, id: 1 
-            expect(response).to render_template(:show)
+        it "if successful, redirect to a show page" do 
+            User.create!(username: "superman", password: "password", id: 1)
+            get :show, params: { id: 1 } 
+            expect(response).to redirect_to(user_url(1))
         end
         it "not successful" do 
             begin

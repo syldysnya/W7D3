a8f59e2478e1a18fca2793a32ae3911c0cd395fc
Daniel <danielwu116@gmail.com>
2021-04-07 16:17:08 -0400

    Write UsersController specs

--- Capies/cute_capies/spec/controllers/users_controller_spec.rb
+++ Capies/cute_capies/spec/controllers/users_controller_spec.rb
@@ -1,3 +1,37 @@
 require 'rails_helper'
 
-RSpec.describe UsersController, type: :controller 
+RSpec.describe UsersController, type: :controller do 
+    describe "#new" do 
+        it "start sign up process" do 
+            get :new
+            expect(response).to render_template(:new)
+        end
+    end
+
+    describe "#create" do 
+        it "with invalid params, renders the new template" do 
+            post :create, user: { username: "superman" }
+            expect(response).to render_template(:new)
+        end
+        it "with valid params, renders the new template" do 
+            post :create, user: { username: "superman", password: "password" }
+            expect(response).to redirect_to(user_url(User.find_by(username: "superman")))
+        end
+    end
+
+    describe "#show" do 
+        it "if successful, renders show page" do 
+            User.create!(username: "superman", password: "password")
+            get :show, id: 1 
+            expect(response).to render_template(:show)
+        end
+        it "not successful" do 
+            begin
+               get :show, id: -1 
+            rescue => exception
+                ActiveRecord::RecordNotFound
+            end
+            expect(response).not_to render_template(:show)
+        end
+    end
+end
